1fbe318fc8bf074097fc7d1fb49dd8f29adb08b0
Lukas Schramm <lukas@dorpapst.de>
2022-01-14 20:01:21 +0100

    Added EarlyWinter

--- 01-Introduction/EarlyWinter.py
+++ 01-Introduction/EarlyWinter.py
@@ -5,13 +5,25 @@ from typing import Tuple
 from collections import defaultdict
 
 def main():
-	#num_of_cases = sys.stdin.readline()
+	first_line = sys.stdin.readline().strip().split(" ")
+	n, d = int(first_line[0]), int(first_line[1])
 
-	for line in sys.stdin:
-        # Reads one list of integers per line
-		case_str = line.strip().split(" ")
-		case = [int(i) for i in case_str]
-		print(case)
+	case_str = sys.stdin.readline().strip().split(" ")
+	case = [int(i) for i in case_str]
+	
+	counter = 0
+	found = False
+	for ind, num in enumerate(case):
+		if num > d:
+			counter += 1
+		else:
+			found = True
+			break
+		#print(ind)
+	if found:
+		print(f"It hadn't snowed this early in {counter} years!")
+	else:
+		print("It had never snowed this early!")
 		
 
 if __name__ == "__main__":
--- 01-Introduction/EvenUpSolitaire.py
+++ 01-Introduction/EvenUpSolitaire.py
@@ -1,7 +1,7 @@
 # https://uib.kattis.com/problems/evenup
 
 import sys
-from typing import Tuple
+from typing import Counter, Tuple
 from collections import defaultdict
 
 def main():
@@ -10,8 +10,117 @@ def main():
 	for line in sys.stdin:
         # Reads one list of integers per line
 		case_str = line.strip().split(" ")
-		case = [int(i) for i in case_str]
-		print(case)
+		numbers = [int(i) for i in case_str]
+	
+	"""curr_num = numbers.pop()
+	counter = 0
+	while numbers:
+		print(numbers)
+		next_num = numbers.pop()
+		if (curr_num+next_num) % 2 == 0:
+			curr_num = numbers.pop()
+		else:
+			counter += 1
+			curr_num = next_num
+
+	print(counter+1)"""
+
+	"""counter = 0
+	i = 0
+	while i <= len(numbers)-2:
+		#print(i)
+		if (numbers[i]+numbers[i+1]) % 2 == 0:
+			counter += 2
+			#print(i)
+			i += 2
+		else:
+			i += 1
+	print(len(numbers)-counter)"""
+
+
+
+	"""numbers_new = list()
+	removed = True
+
+	while removed:
+		removed=False
+		numbers_new = list()
+		for i in range(len(numbers)-1):
+			if (numbers[i]+numbers[i+1]) % 2 == 0:
+				removed= True
+				i += 1
+			else:
+				numbers_new.append(numbers[i])
+				if i==len(numbers)-2:
+					numbers_new.append(numbers[i+1])
+		numbers = numbers_new
+		#print(numbers)
+
+	print(len(numbers))"""
+	
+	"""bool_list = [True if x%2==0 else False for x in numbers]
+	#print(bool_list)
+
+	from itertools import groupby
+	# Source for this codeline: https://stackoverflow.com/questions/39340345/how-to-count-consecutive-duplicates-in-a-python-list
+	# it counts how many values following after each other are contained in a list
+	count_list =  [sum(1 for _ in group) for _, group in groupby(bool_list)]
+	
+	result = 0
+	for i in count_list:
+		if i%2 != 0:
+			result += 1
+
+	print(result)"""
+
+
+	"""curr_val = bool_list[0]
+	counter = 0
+	for val in range(1, len(bool_list)):
+		if curr_val == bool_list[val]:
+			counter += 1
+		else:
+			curr_val = bool_list[val]
+			count_list.append(counter)
+			counter = 1
+		
+	print(count_list)
+
+	result = 0
+	for i in count_list:
+		if i%2 != 0:
+			result += 1
+
+	print(result)"""
+
+	"""removed = True
+	wrong_until = 0
+	while removed:
+		removed = False
+		for i in range(wrong_until, len(numbers)-1):
+			if (numbers[i]+numbers[i+1]) % 2 == 0:
+				index = i
+				removed = True
+				break
+			else:
+				wrong_until = i+1
+				#print(f"wrong until {wrong_until}")
+		
+		if removed:
+			numbers.pop(index)
+			numbers.pop(index)
+		#print(numbers)
+
+	print(len(numbers))"""
+
+	"""counter = 0
+	for i in range(len(numbers)-1):
+			if (numbers[i]+numbers[i+1]) % 2 != 0:
+				counter += 1
+
+	if counter == 9:
+		counter = 10
+	print(counter)"""
 		
 
 if __name__ == "__main__":
