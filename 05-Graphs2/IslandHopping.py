52976df3fed184d986979e833a69d9d00b03d38d
Lukas Schramm <lukas@dorpapst.de>
2022-02-12 15:06:58 +0100

    IslandHopping time limit exceeded

---
+++ 05-Graphs2/IslandHopping.py
@@ -0,0 +1,51 @@
+# https://uib.kattis.com/problems/islandhopping
+
+import sys
+from collections import defaultdict
+from queue import PriorityQueue
+import math
+
+def main():
+	num_of_cases = int(sys.stdin.readline())
+
+	for _ in range(num_of_cases):
+		num_of_islands = int(sys.stdin.readline())
+
+		endpoints = list()
+		for _ in range(num_of_islands):
+			endpoints.append([float(i) for i in sys.stdin.readline().strip().split(" ")])
+
+		adj_list = defaultdict(lambda: [])
+		weight_list = dict()
+		for idx_a, a in enumerate(endpoints):
+			for idx_b, b in enumerate(endpoints):
+				if idx_a < idx_b:
+					dist = math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+					adj_list[idx_a].append(idx_b)
+					adj_list[idx_b].append(idx_a)
+					weight_list[(idx_a, idx_b)] = dist
+					weight_list[(idx_b, idx_a)] = dist
+
+		pq = PriorityQueue()
+		visited = defaultdict(lambda: False)
+		visited[0] = True
+		for neighbour in adj_list[0]:
+			pq.put((weight_list[0, neighbour], neighbour))
+
+		edge_sum = 0
+		while not pq.empty():
+			next_smallest_node = pq.get()
+
+			if not visited[next_smallest_node[1]]:
+				visited[next_smallest_node[1]] = True
+				edge_sum += next_smallest_node[0]
+				for neighbour in adj_list[next_smallest_node[1]]:
+					if not visited[neighbour]:
+						pq.put((weight_list[next_smallest_node[1],neighbour], neighbour))
+		
+		print(f"{edge_sum}")
+
+		
+
+if __name__ == "__main__":
+	main()
