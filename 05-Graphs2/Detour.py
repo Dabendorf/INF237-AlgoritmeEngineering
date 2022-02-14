e691916da8c95d282af39ba11cf932d2dcd2223f
Lukas Schramm <lukas@dorpapst.de>
2022-02-14 15:56:32 +0100

    New wrong solution detour

--- 05-Graphs2/Detour.py
+++ 05-Graphs2/Detour.py
@@ -12,8 +12,8 @@ import heapq
 	"""
 def main():
 	n, m = [int(i) for i in sys.stdin.readline().strip().split(" ")]
-	print(n)
-	print(m)
+	#print(n)
+	#print(m)
 
 	adj_list = defaultdict(lambda: [])
 	for _ in range(m):
@@ -21,10 +21,11 @@ def main():
 		adj_list[i].append((j, cost))
 		adj_list[j].append((i, cost))
 
-	print(f"Adj: {dict(adj_list)}")
+	#print(f"Adj: {dict(adj_list)}")
 
 	adj_list = dijkstra_remover(adj_list, 1, 0, n)
-	print(dijkstra(adj_list, 0, 1, n))
+	#print(dijkstra(adj_list, 0, 1, n))
+	print(bfs(0, adj_list, n, 1))
 
 def dijkstra_remover(adj_list, start, end, num_of_cities):
 	# Priority Queue
@@ -63,25 +64,69 @@ def dijkstra_remover(adj_list, start, end, num_of_cities):
 					heapq.heappush(q, (v[1], v[0]))
 
 
-	print("Visited:")
+	"""print("Visited:")
 	print(visited)
 	print("Distances: ")
 	print(dist)
 	print("Pre")
-	print(pre)
+	print(pre)"""
 
 	new_adj_list = defaultdict(lambda: [])
 
 	for k, v in adj_list.items():
 		for el in v:
-			print(k, el)
+			#print(k, el)
 			if el[0] != pre[k]:
-				new_adj_list[k].append(el)
+				new_adj_list[k].append(el[0])
+				#print(el)
 
-	print(new_adj_list)
+	#print(new_adj_list)
 	return new_adj_list
 
-def dijkstra(adj_list, start, end, num_of_cities):
+def bfs(s, adj_list, num_of_cities, end):
+	""" Its a BFS. The way a BFS always has behaved"""
+	output_list = set()
+	visited = defaultdict(lambda: False)
+
+	# Queue starts with start node
+	queue = [s]
+	visited[s] = True
+	visitedFrom = [None] * num_of_cities
+
+	# While queue, go through them
+	while queue:
+		s = queue.pop()
+
+		if s != None:
+			output_list.add(s)
+
+		# Mark neighbours as visited and add to queue
+		for i in adj_list[s]:
+			if visited[i] == False:
+				queue.append(i)
+				visitedFrom[i] = s
+				visited[i] = True
+
+	# Returns list of all visited nodes
+	#print(f"Klinger: {visitedFrom}")
+
+	if not visited[1]:
+		return "impossible"
+	else:
+		counter_len = 1
+		curr = 1
+		output_str = "1"
+
+		while curr != 0:
+			curr = visitedFrom[curr]
+			output_str = str(curr)+" "+output_str
+			counter_len += 1
+			#print(output_str)
+		return str(counter_len)+ " "+output_str
+
+	#return output_list
+
+"""def dijkstra(adj_list, start, end, num_of_cities):
 	# Priority Queue
 	q = []
 	num_of_nodes = num_of_cities
@@ -137,7 +182,7 @@ def dijkstra(adj_list, start, end, num_of_cities):
 			output_str += " "+str(curr)
 			counter_len += 1
 
-		return str(counter_len)+ " "+output_str
+		return str(counter_len)+ " "+output_str"""
 
 if __name__ == "__main__":
 	main()
