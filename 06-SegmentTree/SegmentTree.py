843ff2715bcadf7d0deb6d2a2ab01b0525a0a975
Lukas Schramm <lukas@dorpapst.de>
2022-02-22 14:10:09 +0100

    SegmentTree code from Pål

---
+++ 06-SegmentTree/SegmentTree.py
@@ -0,0 +1,58 @@
+from re import I
+import sys
+from typing import Tuple
+from collections import defaultdict
+
+def main():
+	data = [3, 5, 6, 2, 0, 9, 8, 1]
+
+	tree = [0] * len(data) + data
+
+	left = lambda i: 2 * i
+	right = lambda i: 2 * i + 1
+	parent = lambda i: i // 2
+	index = lambda T, i: len(T) // 2 + i
+	
+	tree2 = fill(tree)
+	
+	print(tree2)
+
+def fill(tree, op=sum):
+	left = lambda i: 2 * i
+	right = lambda i: 2 * i + 1
+
+	internal = range(1, len(tree) // 2)
+	for idx in reversed(internal): # internal nodes backwards
+		tree[idx] = op((tree[left(idx)], tree[right(idx)]))
+
+	return tree
+
+def update(tree, idx, value, op=sum):
+	left = lambda i: 2 * i
+	right = lambda i: 2 * i + 1
+	parent = lambda i: i // 2
+
+	tree[idx] = value
+	while (idx := parent(idx)) > 0:
+		tree[idx] = op((tree[left(idx)], tree[right(idx)]))
+
+def query_(T, l, r):
+	left = lambda i: 2 * i
+	right = lambda i: 2 * i + 1
+	parent = lambda i: i // 2
+
+	yield T[l] # [l, r)
+	while True:
+		pl = parent(l)
+		pr = parent(r)
+		if pl == pr:
+			return
+		if l % 2 == 0:
+			yield T[right(pl)]
+		if r % 2 == 1:
+			yield T[left(pr)]
+		l,r = pl, pr
+		
+
+if __name__ == "__main__":
+	main()
