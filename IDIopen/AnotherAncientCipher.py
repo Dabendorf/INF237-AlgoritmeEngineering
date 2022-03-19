145304a78e8f5aa978f6dead2e762b361cf7d74c
Lukas Schramm <lukas@dorpapst.de>
2022-03-19 14:45:11 +0100

    AncientCipher not working

--- IDIopen/AnotherAncientCipher.py
+++ IDIopen/AnotherAncientCipher.py
@@ -18,28 +18,22 @@ def augustus(todecode, key, azlist, letter_ind):
 	output_string = ""
 	print(f"Len key: {len(key)}")
 	for idx, letter in enumerate(todecode):
-		ord_num_of_letter = letter_ind[letter]
+		ord_num_of_letter = letter_ind[key[idx_temp]]
 
 		idx_temp = idx%len(key)
 		print(f"idx: {idx} {idx_temp} {key[idx_temp]}")
 		print(f"letter: {letter} {letter_ind[letter]}")
+
 		if ord_num_of_letter % 2 == 0:
-			#print(-letter_ind[key[idx_temp]])
 			output_letter = caesar(letter, -letter_ind[key[idx_temp]], azlist, letter_ind)
 			output_string += output_letter
 		else:
-			#print(letter_ind[key[idx_temp]])
 			output_letter = caesar(letter, +letter_ind[key[idx_temp]], azlist, letter_ind)
 			output_string += output_letter
 	return output_string
 
-def caesar(todecode, key, azlist, letter_ind):
-	new_message = ""
-	for letter in todecode:
-		#print((letter_ind[letter]+key) % 26)
-		new_message += azlist[((letter_ind[letter]+key) % 26)]
-	
-	return new_message
+def caesar(letter, key, azlist, letter_ind):
+	return azlist[((letter_ind[letter]+key) % 26)]
 
 if __name__ == "__main__":
 	main()
