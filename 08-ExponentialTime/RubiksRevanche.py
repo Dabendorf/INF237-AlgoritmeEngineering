21dae75df601f13767042a607fb2ff6313e07b26
Lukas Schramm <lukas@dorpapst.de>
2022-03-21 21:55:31 +0100

    RubiksRevanche non working depth search

--- 08-ExponentialTime/RubiksRevanche.py
+++ 08-ExponentialTime/RubiksRevanche.py
@@ -8,16 +8,82 @@ else:
 
 def main():
 	orig_positions = list()
+	goal = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3]
 	letter_to_num = {"R":0, "G":1, "B":2, "Y":3}
 
 	for i in range(4):
 		line = [letter_to_num[i] for i in list(sys.stdin.readline().strip())]
 		orig_positions.extend(line)
 
-	orig_positions = list(range(16))
 	debug(orig_positions)
-	for i in neighbours(orig_positions):
-		print(i)
+	#for i in neighbours(orig_positions):
+
+	print(bidirectional_search(orig_positions, goal))
+
+def bidirectional_search(start, goal):
+	queue_start = [start]
+	queue_goal = [goal]
+
+	visited_start = [start]
+	visited_goal = [goal]
+
+	"""depth_start = dict()
+	depth_goal = dict()
+	depth_start[tuple(start)] = 0
+	depth_goal[tuple(end)] = 0"""
+
+	while queue_start and queue_goal:
+		s = queue_start.pop()
+		#depth_s = depth_start[tuple(s)]
+		
+		if s == goal or s in queue_goal:
+			#print(depth_start[tuple(s)]+depth_goal[tuple(s)])
+			return True
+
+		for u in neighbours(s):
+			if not u in visited_start:
+				#depth_start[tuple(u)] = depth_s +1
+				queue_start.append(u)
+				visited_start.append(u)
+
+		t = queue_goal.pop()
+		#depth_t = depth_goal[tuple(t)]
+
+		if t == start or t in queue_start:
+			#print(depth_start[tuple(s)]+depth_goal[tuple(s)])
+			return True
+
+		for v in neighbours(t):
+			if not v in visited_goal:
+				#depth_goal[tuple(v)] = depth_t +1
+				queue_goal.append(v)
+				visited_goal.append(v)
+	return False
+
+def bfs(s, adj_list):
+	""" Its a BFS. The way a BFS always has behaved"""
+	output_list = set()
+	visited = defaultdict(lambda: False)
+
+	# Queue starts with start node
+	queue = [s]
+	visited[s] = True
+
+	# While queue, go through them
+	while queue:
+		s = queue.pop()
+
+		if s != None:
+			output_list.add(s)
+
+		# Mark neighbours as visited and add to queue
+		for i in adj_list[s]:
+			if visited[i] == False:
+				queue.append(i)
+				visited[i] = True
+
+	# Returns list of all visited nodes
+	return output_list
 
 def swap_positions(pos_old, a, b, c, d, left):
 	pos = pos_old.copy()
