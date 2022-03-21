e1a4c18342c3688df8375fc8673fb1ed153d7786
Lukas Schramm <lukas@dorpapst.de>
2022-03-21 09:02:29 +0100

    SocialAdvertising Wrong answer

--- 08-ExponentialTime/SocialAdvertising.py
+++ 08-ExponentialTime/SocialAdvertising.py
@@ -1,6 +1,7 @@
 import sys
 from typing import Tuple
 from collections import defaultdict
+import itertools
 
 """ Problem
 
@@ -10,15 +11,60 @@ from collections import defaultdict
 def main():
 	num_of_cases = int(sys.stdin.readline())
 
+	# Read every testcase
 	for _ in range(num_of_cases):
 		sets = list(set())
 		num_of_people = int(sys.stdin.readline())
+		
+		# Generate friendlists
 		for person_idx in range(num_of_people):
 			person_friends = set([int(i) for i in sys.stdin.readline().strip().split(" ")][1:])
 			person_friends.add(person_idx+1)
 			sets.append(person_friends)
-		print(sets)
-		
+
+		# Calculate numbers (binary strings) for friend lists
+		numbers = set()
+		for s in sets:
+			num = 0
+			for idx in range(num_of_people):
+				if idx+1 in s:
+					num += (2**(idx))
+			numbers.add(num)
+
+		# Remove sets included in other sets
+		to_remove = set()
+		for idx_a, a in enumerate(numbers):
+			for idx_b, b in enumerate(numbers):
+				if idx_a != idx_b:
+					if a&b == a:
+						to_remove.add(a)
+					elif a&b == b:
+						to_remove.add(b)
+		numbers = numbers.difference(to_remove)
+
+		num_of_sets = len(list(numbers))
+		# Calculate number to aim for
+		number_searched = 2**(num_of_people)-1
+
+		found = False
+		# Iterate through all set combinations from size 1 to num_of_sets
+		# Terminate if found or iterations over
+		for i in range(1, num_of_sets):
+			iterator = itertools.combinations(numbers, i)
+			next_subset = next(iterator)
+
+			# Logical or over sets
+			overall = 0
+			for el in next_subset:
+				overall |= el
+			
+			# Check break condition
+			if overall == number_searched:
+				print(i)
+				found = True
+				break
+		if not found:
+			print(num_of_sets)
 
 if __name__ == "__main__":
 	main()
