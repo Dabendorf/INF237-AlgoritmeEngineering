d1fff5d790551f4174b1579fa222cf785af378b0
Lukas Schramm <lukas@dorpapst.de>
2022-03-22 13:17:52 +0100

    RubiksRevanche Bit calculator

--- 08-ExponentialTime/RubiksRevancheBinshift.py
+++ 08-ExponentialTime/RubiksRevancheBinshift.py
@@ -30,10 +30,10 @@ def main():
 	orig_positions = list_to_bin(orig_positions_temp)
 	goal_positions = list_to_bin(goal_temp)
 
-	printBinBlocks(orig_positions)
-	printBinBlocks(goal_positions)
+	#printBinBlocks(orig_positions)
+	#printBinBlocks(goal_positions)
 
-	#print(bidirectional_search(orig_positions, goal_positions))
+	print(bidirectional_search(orig_positions, goal_positions))
 
 def list_to_bin(l):
 	out = l[0]
@@ -43,87 +43,77 @@ def list_to_bin(l):
 	return out
 
 def bidirectional_search(start, goal):
-	visited_start = list()
-	visited_end = list()
+	visited_start = set()
+	visited_end = set()
 	parent_start = dict()
 	parent_end = dict()
 
 	queue_start = [start]
 	queue_goal = [goal]
 
-	visited_start.append(start)
-	visited_end.append(goal)
+	visited_start.add(start)
+	visited_end.add(goal)
 
 	meeting_point = None # later used together with parent to find the depth
 
 	while queue_start and queue_goal and meeting_point is None:
 		s = queue_start.pop()
 		for u in neighbours(s):
-			print(f"neighbour u, visited: {len(visited_start)}")
+			#print(f"neighbour u, visited: {len(visited_start)}")
 			if not u in visited_start:
 				queue_start.append(u)
-				visited_start.append(u)
-				parent_start[tuple(u)] = s
+				visited_start.add(u)
+				parent_start[u] = s
 
 		if s == goal or s in queue_goal:
 			return True
 
 		t = queue_goal.pop()
 		for v in neighbours(t):
-			print(f"neighbour v, visited: {len(visited_end)}")
+			#print(f"neighbour v, visited: {len(visited_end)}")
 			if not v in visited_end:
 				queue_goal.append(v)
-				visited_end.append(v)
-				parent_end[tuple(v)] = t
+				visited_end.add(v)
+				parent_end[v] = t
 
 		if t == start or s in queue_start:
 			return True
 
+def swap_positions(pattern, ind0, ind1, ind2, ind3, left):
+	mask0 = 3 << (16-ind0-1)*2
+	mask1 = 3 << (16-ind1-1)*2
+	mask2 = 3 << (16-ind2-1)*2
+	mask3 = 3 << (16-ind3-1)*2
 
-def bfs(s, adj_list):
-	""" Its a BFS. The way a BFS always has behaved"""
-	output_list = set()
-	visited = defaultdict(lambda: False)
+	val0 = pattern & mask0
+	val1 = pattern & mask1
+	val2 = pattern & mask2
+	val3 = pattern & mask3
 
-	# Queue starts with start node
-	queue = [s]
-	visited[s] = True
+	pattern = pattern & ~mask0
+	pattern = pattern & ~mask1
+	pattern = pattern & ~mask2
+	pattern = pattern & ~mask3
 
-	# While queue, go through them
-	while queue:
-		s = queue.pop()
-
-		if s != None:
-			output_list.add(s)
-
-		# Mark neighbours as visited and add to queue
-		for i in adj_list[s]:
-			if visited[i] == False:
-				queue.append(i)
-				visited[i] = True
-
-	# Returns list of all visited nodes
-	return output_list
-
-def swap_positions(pos_old, a, b, c, d, left):
-	pos = pos_old.copy()
+	val0 >>= (16-ind0-1)*2
+	val1 >>= (16-ind1-1)*2
+	val2 >>= (16-ind2-1)*2
+	val3 >>= (16-ind3-1)*2
 
 	if left:
-		swap = pos[a]
-		pos[a] = pos[b]
-		pos[b] = pos[c]
-		pos[c] = pos[d]
-		pos[d] = swap
+		pattern |= val0 << (16-ind1-1)*2
+		pattern |= val1 << (16-ind2-1)*2
+		pattern |= val2 << (16-ind3-1)*2
+		pattern |= val3 << (16-ind0-1)*2
 	else:
-		swap = pos[d]
-		pos[d] = pos[c]
-		pos[c] = pos[b]
-		pos[b] = pos[a]
-		pos[a] = swap
-	return pos
+		pattern |= val0 << (16-ind3-1)*2
+		pattern |= val1 << (16-ind0-1)*2
+		pattern |= val2 << (16-ind1-1)*2
+		pattern |= val3 << (16-ind2-1)*2
 
-def neighbours(orig_positions):
-	pos = orig_positions.copy()
+	return pattern
+
+def neighbours(pos):
 	yield swap_positions(pos, 0,1,2,3, False)
 	yield swap_positions(pos, 4,5,6,7, False)
 	yield swap_positions(pos, 8,9,10,11, False)
