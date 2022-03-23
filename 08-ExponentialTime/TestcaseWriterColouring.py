e3a2475b5f524e4b0d52686ce0b96964284c663d
Lukas Schramm <lukas@dorpapst.de>
2022-03-23 16:45:33 +0100

    Change Testcasegenerator mapcolouring

--- 08-ExponentialTime/TestcaseWriterColouring.py
+++ 08-ExponentialTime/TestcaseWriterColouring.py
@@ -1,12 +1,12 @@
 from random import randint
 
-num_test_cases = 1
+num_test_cases = 20
 print(num_test_cases)
 
 for _ in range(num_test_cases):
-	num_nodes = randint(1, 4)
+	num_nodes = randint(5, 16)
 	if num_nodes!=1:
-		num_edges = randint(0, (num_nodes * (num_nodes-1))/2-1)
+		num_edges = randint(1, (num_nodes * (num_nodes-1))/2-1)
 	else:
 		num_edges = 0
 
@@ -14,6 +14,8 @@ for _ in range(num_test_cases):
 
 	for i in range(num_edges):
 		node_a = randint(0, num_nodes-1)
-		node_b = randint(0, num_nodes-1)
+		node_b = node_a
+		while node_b == node_a:
+			node_b = randint(0, num_nodes-1)
 
 		print(f"{node_a} {node_b}")
