2671823eb17b86e5fcdd83b5db6916c54293785a
Lukas Schramm <lukas@dorpapst.de>
2022-04-03 18:20:54 +0200

    Add one wrong answer and one TLE for graphs 3

---
+++ 10-Graphs3/Paintball.py
@@ -0,0 +1,102 @@
+# https://open.kattis.com/problems/paintball
+
+from collections import defaultdict, deque
+import sys
+
+class Graph:
+	edges = defaultdict(lambda: defaultdict(lambda: None))
+	R = defaultdict(lambda: defaultdict(lambda: 0))
+
+	def __init__(self, orig_cap, V):
+		for k in orig_cap:
+			a, b, w = k
+			self.edges[a][b] = w
+			self.R[a][b] = w
+		self.V = V
+
+def main():
+	players, edges = list(map(int, sys.stdin.readline().strip().split()))
+
+	# Read original graph into original capacities, every node exists two times as "a" and "b" (bipartite)
+	orig_cap = list()
+	for _ in range(edges):
+		A, B = list(map(int, sys.stdin.readline().strip().split()))
+		orig_cap.append((f"{A}a", f"{B}b", 1))
+		orig_cap.append((f"{B}a", f"{A}b", 1))
+		
+	# Add s and t including edges to all of them
+	V = list()
+	for i in range(1, players+1):
+		orig_cap.append(("s", f"{i}a", 1))
+		orig_cap.append((f"{i}b", "t", 1))
+		V.append(f"{i}a")
+		V.append(f"{i}b")
+
+	V.append("s")
+	V.append("t")
+
+	graph = Graph(orig_cap, V)
+	s = "s"
+	t = "t"
+
+	# Run max-flow
+	mflow = maxflow(graph, s, t)
+	"""print(f"V: {V}")
+	print(f"Cap: {orig_cap}")
+	print(f"Max flow: {mflow}")"""
+
+	count_edges = 0
+	output = [0] * players
+
+	# Go through all original edges and look for opposite edge of residual graph
+	for fra, v in graph.edges.items():
+		for til, weight in v.items():
+			val_res = graph.R[til][fra]
+			if val_res != 0 and fra != "s" and til != "t":
+				count_edges += 1
+				output[int(fra[:-1])-1] = til[:-1]
+
+	if count_edges == players:
+		print("\n".join(output))
+	else:
+		print("impossible")
+
+edges = lambda p: zip(p, p[1:])
+
+def bfs(graph, s, t):
+	q = deque([s])
+	parent = {}
+	while q:
+		v = q.popleft()
+		for u in graph.V: # !
+			if u in parent:
+				continue # seen it before
+			if graph.R[v][u] <= 0:
+				continue # vu saturated
+			parent[u] = v
+			q.append(u)
+			if u == t:
+				return create_path(parent, s, t)
+
+def create_path(parent, s, t):
+	path = [t]
+	while t != s:
+		t = parent[t]
+		path.append(t)
+	return tuple(reversed(path))
+
+def maxflow(graph, s, t):
+	flow = 0
+	P = bfs(graph, s, t)
+	while P:
+		F = min(graph.R[v][u] for (v, u) in edges(P))
+		flow += F
+		for i in range(1, len(P)):
+			v, u = P[i - 1], P[i]
+			graph.R[v][u] -= F
+			graph.R[u][v] += F
+		P = bfs(graph, s, t)
+	return flow
+
+if __name__ == "__main__":
+	main()
---
+++ 10-Graphs3/PianoLessons.py
@@ -0,0 +1,94 @@
+# https://open.kattis.com/problems/pianolessons
+
+# https://open.kattis.com/problems/paintball
+
+from collections import defaultdict, deque
+import sys
+
+class Graph:
+	edges = defaultdict(lambda: defaultdict(lambda: None))
+	R = defaultdict(lambda: defaultdict(lambda: 0))
+
+	def __init__(self, orig_cap, V):
+		for k in orig_cap:
+			a, b, w = k
+			self.edges[a][b] = w
+			self.R[a][b] = w
+		self.V = V
+
+def main():
+	students, timeslots = list(map(int, sys.stdin.readline().strip().split()))
+
+	# Read original graph into original capacities, every node exists two times as "a" and "b" (bipartite)
+	orig_cap = list()
+	V = list()
+
+	for idx_student in range(students):
+		student_inf = list(map(int, sys.stdin.readline().strip().split()))
+		num_of_lessons = student_inf[0]
+
+		if num_of_lessons > 0:
+			for i in range(1, len(student_inf)):
+				orig_cap.append((f"{idx_student+1}a", f"{student_inf[i]}b", 1))
+			
+			orig_cap.append(("s", f"{idx_student+1}a", 1))
+			V.append(f"{idx_student+1}a")
+
+	for idx_timeslots in range(timeslots):
+		orig_cap.append((f"{idx_timeslots+1}b", "t", 1))
+		V.append(f"{idx_timeslots+1}b")
+
+	V.append("s")
+	V.append("t")
+
+	#print(V)
+	#print(orig_cap)
+
+	graph = Graph(orig_cap, V)
+	s = "s"
+	t = "t"
+
+	# Run max-flow
+	mflow = maxflow(graph, s, t)
+
+	print(mflow)
+
+edges = lambda p: zip(p, p[1:])
+
+def bfs(graph, s, t):
+	q = deque([s])
+	parent = {}
+	while q:
+		v = q.popleft()
+		for u in graph.V: # !
+			if u in parent:
+				continue # seen it before
+			if graph.R[v][u] <= 0:
+				continue # vu saturated
+			parent[u] = v
+			q.append(u)
+			if u == t:
+				return create_path(parent, s, t)
+
+def create_path(parent, s, t):
+	path = [t]
+	while t != s:
+		t = parent[t]
+		path.append(t)
+	return tuple(reversed(path))
+
+def maxflow(graph, s, t):
+	flow = 0
+	P = bfs(graph, s, t)
+	while P:
+		F = min(graph.R[v][u] for (v, u) in edges(P))
+		flow += F
+		for i in range(1, len(P)):
+			v, u = P[i - 1], P[i]
+			graph.R[v][u] -= F
+			graph.R[u][v] += F
+		P = bfs(graph, s, t)
+	return flow
+
+if __name__ == "__main__":
+	main()
